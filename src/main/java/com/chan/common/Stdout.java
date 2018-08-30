b88e5eff8236c6e63d951fb9e73c0a9581f1c8d6
yuyuanhe <yuyuanhe@xiaomi.com>
2018-08-30 14:48:19 +0800

    add singleton class

---
+++ src/main/java/com/chan/common/Stdout.java
@@ -0,0 +1,12 @@
+package com.chan.common;
+
+/**
+ * Created by yuyuanhe
+ * Date: 8/30/18 2:42 PM
+ * Description:
+ */
+public class Stdout {
+    public static void println(Object oj) {
+        System.out.println(oj);
+    }
+}
---
+++ src/main/java/com/chan/design/pattern/Singleton.java
@@ -0,0 +1,35 @@
+package com.chan.design.pattern;
+
+import com.chan.common.Stdout;
+
+/**
+ * Created by yuyuanhe
+ * Date: 8/30/18 2:37 PM
+ * Description:
+ */
+public class Singleton {
+    private static volatile Singleton instance;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton instance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.instance();
+        Singleton singleton1 = Singleton.instance();
+
+        Stdout.println("addr1 " + singleton + " addr2" + singleton1);
+        Stdout.println(singleton.equals(singleton1));
+    }
+}
